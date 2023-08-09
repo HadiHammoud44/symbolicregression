5a67bf26e3212678e3a0f9457e97da9d2cf46f2f
Hadi Hammoud <hadi.hammoud@epfl.ch>
2023-08-10 02:30:47 +0300

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import pathlib
+
+
+dir = pathlib.Path(__file__).resolve().parent
+with open(dir / 'README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='symbolicregression',
+    version='1.0',             
+    description='Symbolic Regression based on LLMs',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Pierre-Alexandre Kamienny, Stéphane d'Ascoli, Guillaume Lample, François Charton",
+    url='https://github.com/facebookresearch/symbolicregression',  
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires=">=3.7",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
+)
